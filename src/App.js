f873bcc54687bb6027701dec255c25b07aef2980
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-12-17 20:51:18 -0600

    set event handler for dropdown selection and added styling via tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    }
+
     const options = [
         {label: 'Fire', value: 'fire'},
         {label: 'Air', value: 'air'},
@@ -8,7 +15,7 @@ function App() {
         {label: 'Earth', value: 'earth'}
     ];
 
-    return <Dropdown options={options}/>
+    return <Dropdown options={options} value={selection} onChange={handleSelect}/>
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
+import { TiChevronLeftOutline } from "react-icons/ti";
 
-function Dropdown({ options }) {
+
+function Dropdown({ options, value, onChange }) {
     const [open, setOpen] = useState(false);
 
     const handleClick = () => {
         setOpen(!open);
     };
 
+    const handleOptClick = (option) => {
+        setOpen(false);
+        onChange(option);
+    };
+
     const renderedOpts = options.map((option) => {
         return (
-            <div key={option.value}>
+            <div className="hover:bg-sky-100 rounder cursor-pointer p-1"
+            onClick={() => handleOptClick(option)} key={option.value}>
                 {option.label}
             </div>
         );
     });
+
     return (
-        <div>  
-            <div onClick={handleClick}>Select Element</div>
-            {open && <div>{renderedOpts}</div>} 
+        <div className="w-48 relative">  
+            <div className="flex justify-between itemso0center cursor-pointer border rounded p-3 shadow bg-white w-full" 
+            onClick={handleClick}>
+                {value?.label || 'Select Element'}
+                <TiChevronLeftOutline className="text-lg -rotate-90" />
+            </div>
+            {open && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOpts}</div>} 
         </div>
     );
 }
